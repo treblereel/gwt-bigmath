8cb893a5dd213ec0e7b2893ee67bf35840a6ed03
Dmitrii Tikhomirov <chani@me.com>
2020-06-24 12:16:02 +0200

    removed useless deps

--- gwt-bigmath-gwt2-tests/src/test/java/org/treblereel/gwt/bigmath/BigComplexMathTest.java
+++ gwt-bigmath-gwt2-tests/src/test/java/org/treblereel/gwt/bigmath/BigComplexMathTest.java
@@ -101,103 +101,140 @@ public class BigComplexMathTest extends GWTTestCase {
 
   @Test
   public void testTan() {
-      assertPrecisionCalculation(
-              BigComplex.valueOf(new BigDecimal("0.03381282607989669028437055972528730164018766933075587436"), new BigDecimal("1.014793616146633568117054175417967614163217471123533242")).round(MC),
-              (mc) -> BigComplexMath.tan(BigComplex.valueOf(1, 2), mc),
-              5);
+    assertPrecisionCalculation(
+        BigComplex.valueOf(
+                new BigDecimal("0.03381282607989669028437055972528730164018766933075587436"),
+                new BigDecimal("1.014793616146633568117054175417967614163217471123533242"))
+            .round(MC),
+        (mc) -> BigComplexMath.tan(BigComplex.valueOf(1, 2), mc),
+        5);
   }
 
   @Test
   public void testLog() {
-      assertPrecisionCalculation(
-              BigComplex.valueOf(new BigDecimal("0.80471895621705018730037966661309381976280067713425886095"), new BigDecimal("1.1071487177940905030170654601785370400700476454014326466")).round(MC),
-              (mc) -> BigComplexMath.log(BigComplex.valueOf(1, 2), mc),
-              5);
+    assertPrecisionCalculation(
+        BigComplex.valueOf(
+                new BigDecimal("0.80471895621705018730037966661309381976280067713425886095"),
+                new BigDecimal("1.1071487177940905030170654601785370400700476454014326466"))
+            .round(MC),
+        (mc) -> BigComplexMath.log(BigComplex.valueOf(1, 2), mc),
+        5);
   }
 
   @Test
   public void testSqrt() {
-      assertPrecisionCalculation(
-              BigComplex.valueOf(new BigDecimal("1.2720196495140689642524224617374914917156080418400962486"), new BigDecimal("0.78615137775742328606955858584295892952312205783772323766")).round(MC),
-              (mc) -> BigComplexMath.sqrt(BigComplex.valueOf(1, 2), mc),
-              5);
+    assertPrecisionCalculation(
+        BigComplex.valueOf(
+                new BigDecimal("1.2720196495140689642524224617374914917156080418400962486"),
+                new BigDecimal("0.78615137775742328606955858584295892952312205783772323766"))
+            .round(MC),
+        (mc) -> BigComplexMath.sqrt(BigComplex.valueOf(1, 2), mc),
+        5);
   }
 
   @Test
   public void testPowInt() {
-      assertPrecisionCalculation(
-              BigComplex.valueOf(new BigDecimal("18456.81197182559"), new BigDecimal("-7537.93433754262")),
-              (mc) -> BigComplexMath.pow(BigComplex.valueOf(1.1, 2.2), 11, mc),
-              5);
+    assertPrecisionCalculation(
+        BigComplex.valueOf(
+            new BigDecimal("18456.81197182559"), new BigDecimal("-7537.93433754262")),
+        (mc) -> BigComplexMath.pow(BigComplex.valueOf(1.1, 2.2), 11, mc),
+        5);
 
-      assertPrecisionCalculation(
-              BigComplex.valueOf(new BigDecimal("0.00004643522633206022281216609142474444925257050125807831"), new BigDecimal("0.00001896457999216310228315702790913198870386323455307511")).round(MC),
-              (mc) -> BigComplexMath.pow(BigComplex.valueOf(1.1, 2.2), -11, mc),
-              5);
+    assertPrecisionCalculation(
+        BigComplex.valueOf(
+                new BigDecimal("0.00004643522633206022281216609142474444925257050125807831"),
+                new BigDecimal("0.00001896457999216310228315702790913198870386323455307511"))
+            .round(MC),
+        (mc) -> BigComplexMath.pow(BigComplex.valueOf(1.1, 2.2), -11, mc),
+        5);
   }
 
   @Test
   public void testPow() {
-      assertPrecisionCalculation(
-              BigComplex.valueOf(new BigDecimal("0.93025840620844374948965939784803335557080436887094399412"), new BigDecimal("2.5254359132812654282958795629575897052282541443174302618")).round(MC),
-              (mc) -> BigComplexMath.pow(BigComplex.valueOf(1.1, 2.2), BigDecimal.valueOf(1.1), mc),
-              5);
+    assertPrecisionCalculation(
+        BigComplex.valueOf(
+                new BigDecimal("0.93025840620844374948965939784803335557080436887094399412"),
+                new BigDecimal("2.5254359132812654282958795629575897052282541443174302618"))
+            .round(MC),
+        (mc) -> BigComplexMath.pow(BigComplex.valueOf(1.1, 2.2), BigDecimal.valueOf(1.1), mc),
+        5);
   }
 
   @Test
   public void testPowComplex() {
-      assertPrecisionCalculation(
-              BigComplex.valueOf(new BigDecimal("0.84004892508974359637454837539731545717714689412422564692"), new BigDecimal("0.25134081792349062667714080016033852435880339768328815847")).round(MC),
-              (mc) -> BigComplexMath.pow(BigComplex.valueOf(1.1, 2.2), BigComplex.valueOf(0.1, 0.2), mc),
-              5);
+    assertPrecisionCalculation(
+        BigComplex.valueOf(
+                new BigDecimal("0.84004892508974359637454837539731545717714689412422564692"),
+                new BigDecimal("0.25134081792349062667714080016033852435880339768328815847"))
+            .round(MC),
+        (mc) -> BigComplexMath.pow(BigComplex.valueOf(1.1, 2.2), BigComplex.valueOf(0.1, 0.2), mc),
+        5);
   }
 
   @Test
   public void testRoot() {
-      assertPrecisionCalculation(
-              BigComplex.valueOf(new BigDecimal("1.2589858696615772964532950314498058030426856758251923734"), new BigDecimal("0.48693816197569980888611771300161883360684382303096712193")).round(MC),
-              (mc) -> BigComplexMath.root(BigComplex.valueOf(1.1, 2.2), BigDecimal.valueOf(3), mc),
-              5);
+    assertPrecisionCalculation(
+        BigComplex.valueOf(
+                new BigDecimal("1.2589858696615772964532950314498058030426856758251923734"),
+                new BigDecimal("0.48693816197569980888611771300161883360684382303096712193"))
+            .round(MC),
+        (mc) -> BigComplexMath.root(BigComplex.valueOf(1.1, 2.2), BigDecimal.valueOf(3), mc),
+        5);
   }
 
   @Test
   public void testRootComplex() {
-      assertPrecisionCalculation(
-              BigComplex.valueOf(new BigDecimal("1.4494235461237517229913668192323085954480223365111721042"), new BigDecimal("0.17040446483770403353362509072059809846680452097000085527")).round(MC),
-              (mc) -> BigComplexMath.root(BigComplex.valueOf(1.1, 2.2), BigComplex.valueOf(3, 2), mc),
-              5);
+    assertPrecisionCalculation(
+        BigComplex.valueOf(
+                new BigDecimal("1.4494235461237517229913668192323085954480223365111721042"),
+                new BigDecimal("0.17040446483770403353362509072059809846680452097000085527"))
+            .round(MC),
+        (mc) -> BigComplexMath.root(BigComplex.valueOf(1.1, 2.2), BigComplex.valueOf(3, 2), mc),
+        5);
   }
 
   @Test
   public void testAsin() {
-      assertPrecisionCalculation(
-              BigComplex.valueOf(new BigDecimal("0.42707858639247612548064688331895685930333615088099242008"), new BigDecimal("1.5285709194809981612724561847936733932886832962358541247")).round(MC),
-              (mc) -> BigComplexMath.asin(BigComplex.valueOf(1, 2), mc),
-              5);
+    assertPrecisionCalculation(
+        BigComplex.valueOf(
+                new BigDecimal("0.42707858639247612548064688331895685930333615088099242008"),
+                new BigDecimal("1.5285709194809981612724561847936733932886832962358541247"))
+            .round(MC),
+        (mc) -> BigComplexMath.asin(BigComplex.valueOf(1, 2), mc),
+        5);
   }
 
   @Test
   public void testAcos() {
-      assertPrecisionCalculation(
-              BigComplex.valueOf(new BigDecimal("1.14371774040242049375067480832079458279524854880656049040"), new BigDecimal("-1.52857091948099816127245618479367339328868329623585412477")).round(MC),
-              (mc) -> BigComplexMath.acos(BigComplex.valueOf(1, 2), mc),
-              5);
+    assertPrecisionCalculation(
+        BigComplex.valueOf(
+                new BigDecimal("1.14371774040242049375067480832079458279524854880656049040"),
+                new BigDecimal("-1.52857091948099816127245618479367339328868329623585412477"))
+            .round(MC),
+        (mc) -> BigComplexMath.acos(BigComplex.valueOf(1, 2), mc),
+        5);
   }
 
   @Test
   public void testAtan() {
-      assertPrecisionCalculation(
-              BigComplex.valueOf(new BigDecimal("1.3389725222944935611241935759091442410843161725444927785"), new BigDecimal("0.40235947810852509365018983330654690988140033856712943047")).round(MC),
-              (mc) -> BigComplexMath.atan(BigComplex.valueOf(1, 2), mc),
-              5);
+    assertPrecisionCalculation(
+        BigComplex.valueOf(
+                new BigDecimal("1.3389725222944935611241935759091442410843161725444927785"),
+                new BigDecimal("0.40235947810852509365018983330654690988140033856712943047"))
+            .round(MC),
+        (mc) -> BigComplexMath.atan(BigComplex.valueOf(1, 2), mc),
+        5);
   }
 
   @Test
   public void testAcot() {
-      assertPrecisionCalculation(
-              BigComplex.valueOf(new BigDecimal("0.23182380450040305810712811573060720101426852714306013190"), new BigDecimal("-0.40235947810852509365018983330654690988140033856712943047")).round(MC),
-              (mc) -> BigComplexMath.acot(BigComplex.valueOf(1, 2), mc),
-              5);
+    assertPrecisionCalculation(
+        BigComplex.valueOf(
+                new BigDecimal("0.23182380450040305810712811573060720101426852714306013190"),
+                new BigDecimal("-0.40235947810852509365018983330654690988140033856712943047"))
+            .round(MC),
+        (mc) -> BigComplexMath.acot(BigComplex.valueOf(1, 2), mc),
+        5);
   }
 
   @Override
